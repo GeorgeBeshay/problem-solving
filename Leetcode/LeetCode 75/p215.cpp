3bb36c656fe7a7d8dcbdeecbb1614bfbbd9d41da
George Beshay <georgebeshay101@gmail.com>
2026-02-13 18:13:31 +0200

    Solved problem 215 from the LeetCode 75 problems list.

---
+++ Leetcode/LeetCode 75/p215.cpp
@@ -0,0 +1,26 @@
+/*
+ * Created By George on 2/13/2026.
+ */
+
+/*
+ * Problem Link:        https://leetcode.com/problems/kth-largest-element-in-an-array/
+ * Problem Status:      Accepted.
+ */
+
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> veci;
+typedef vector<vector<int> > vveci;
+typedef pair<int, int> pr;
+
+#define ffor(i, n, j, m) for(int i = 0 ; i < n ; i++)for(int j = 0 ; j < m ; j++)
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MAX_INT numeric_limits<int>::max()
+
+int main() {
+    return 0;
+}
